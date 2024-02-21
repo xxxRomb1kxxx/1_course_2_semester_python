59ca073968ea649453558f182d11ef978db24e38
Romb1k <144805186+xxxRomb1kxxx@users.noreply.github.com>
2024-02-21 22:16:28 +0200

    Add files via upload

---
+++ lab2_task2/lab2_task2.py
@@ -0,0 +1,17 @@
+from math import factorial
+rows = int(input(""))
+for i in range(rows):
+    for j in range(rows-i+1):
+        print(end=" ")
+    for j in range(i+1):
+        nCr = factorial(i) // (factorial(j) * factorial(i-j))
+        if nCr % 2 == 0:
+            print(" ",end = " ")
+        else:
+            print("*",end = " ")
+        if j == i+1:
+            print("*",end = " ")
+    print()
+for i in range(rows+2):
+    print("*", end=" ")
+  
