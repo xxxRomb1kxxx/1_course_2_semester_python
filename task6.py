1755c25db245ab789029ae1b6756e82dd9f550b3
Romb1k <144805186+xxxRomb1kxxx@users.noreply.github.com>
2024-03-06 23:37:50 +0200

    Delete task6.py

--- task6.py
+++
@@ -1,8 +0,0 @@
-string = "money money money"
-def acronym(phrase):
-    words = phrase.split()
-    acronym = ""
-    for word in words:
-        acronym += word[0].upper()
-    return acronym
-print(string, acronym(string))
