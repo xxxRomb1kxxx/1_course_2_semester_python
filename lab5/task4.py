f7e060c6f1d88fd773bce2edbd719adb10716dbf
Romb1k <144805186+xxxRomb1kxxx@users.noreply.github.com>
2024-04-03 19:50:55 +0200

    Update task4.py

--- lab5/task4.py
+++ lab5/task4.py
@@ -1,35 +1,3 @@
-﻿# import json
-# import csv
-# import os
-# import sys
-
-# def json_to_csv(json_file):
-#     json_path = os.path.abspath(json_file)
-#     json_name = os.path.splitext(os.path.basename(json_path))[0]
-#     with open(json_file, 'r', encoding='utf-8') as f:
-#         data = json.load(f)
-#     csv_name = f"{json_name}.csv"
-#     csv_path = os.path.join(os.path.dirname(json_path), csv_name)
-#     with open(csv_path, 'w', newline='', encoding='utf-8') as csvfile:
-#         writer = csv.writer(csvfile)
-#         if isinstance(data, list):
-#             if len(data) > 0:
-#                 writer.writerow(data[0].keys())
-#                 for item in data:
-#                     writer.writerow(item.values())
-#         elif isinstance(data, dict):
-#             writer.writerow(data.keys())
-#             writer.writerow(data.values())
-
-#     print(f"CSV файл успешно создан: {csv_path}")
-
-# if __name__ == "__main__":
-#     if len(sys.argv) != 2:
-#         print("Использование: python json2csv.py example.json")
-#         sys.exit(1)
-
-#     json_file = sys.argv[1]
-#     json_to_csv(json_file)
 import json
 import csv
 import os
